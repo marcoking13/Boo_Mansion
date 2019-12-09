8c521de784ab03410f8ef1edec8c97d5bbe64813
marcoking13 <marcokhodr16@gmail.com>
2019-12-09 04:41:28 -0800

    fixed score box

--- src/components/end/end_box.js
+++ src/components/end/end_box.js
@@ -11,27 +11,30 @@ render(){
          <h1 className="text-center cr bb monospacebold end mlop">{this.props.data.title}</h1>
           <br />
          <div className="mt5 row">
-         <div className="col-4"/>
-         <div className="col-4 bb mt10 b10px ml35 pb5 scoreBox">
+         <div className="col-3"/>
+         <div className="col-6 bb mt10 b10px ml35 pb5 scoreBox">
             <br />
             <br />
+            <div className="row">
+              <div className="col-6">
 
-           <h3 className="text-center co bold scoreFinal lls">
-            <img className="icoE ico2"src="images/time.png"/>
-              Time:
-              <strong  className="cy"> {this.props.data.time}</strong>
-            </h3>
+                <h3 className="text-center co bold scoreFinal lls">
+                  <img className="icoE ico2"src="images/time.png"/>
+                  Time:
+                    <strong  className="cy"> {this.props.data.time}</strong>
+                </h3>
             <br />
-
-          <h3 className="text-center co bold scoreFinal">
-            <img className="posRel icoE boE"src="images/logo.png"/>
-              Kills:
-              <strong className="cy"> {this.props.data.kills}</strong>
-            </h3>
+          </div>
+          <div className="col-6">
+            <h3 className="text-center co bold scoreFinal">
+              <img className="posRel icoE boE"src="images/logo.png"/>
+                Kills:
+                <strong className="cy"> {this.props.data.kills}</strong>
+              </h3>
+            </div>
+          </div>
 
           </div>
-          
-         </div>
          <br />
          <br />
          <h1 className="backToMapTxt text-center sigmoreFont cy "  onClick = {()=>{this.props.pageChange("start",null)}}  >Main Menu</h1>
