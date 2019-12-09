d399987ac1880a0e48aef544fece53fb7c67b98b
marcoking13 <marcokhodr16@gmail.com>
2019-12-09 05:07:49 -0800

    remade end box

--- src/components/end/end_box.js
+++ src/components/end/end_box.js
@@ -6,40 +6,64 @@ class EndBox extends React.Component {
 render(){
    return(
 
-     <div className="finalSheet container-fluid" style={{background:"url('images/bb.png')"}}>
-
+     <div className="container-fluid" style={{background:"url('images/bb.png')"}}>
+        <br />
+        <br />
          <h1 className="text-center cr bb monospacebold end mlop">{this.props.data.title}</h1>
           <br />
          <div className="mt5 row">
-         <div className="col-3"/>
-         <div className="col-6 bb mt10 b10px ml35 pb5 scoreBox">
-            <br />
-            <br />
-            <div className="row">
-              <div className="col-6">
 
-                <h3 className="text-center co bold  lls">
-                  <img className="icoE ico2"src="images/time.png"/>
-                  Time:
-                    <strong  className="cy"> {this.props.data.time}</strong>
-                </h3>
-            <br />
-          </div>
-          <div className="col-6">
-            <h3 className="text-center co bold ">
-              <img className="posRel icoE boE"src="images/logo.png"/>
-                Kills:
-                <strong className="cy"> {this.props.data.kills}</strong>
-              </h3>
-            </div>
-          </div>
 
-          </div>
-         <br />
-         <br />
-         <h1 className="backToMapTxt text-center sigmoreFont cy "  onClick = {()=>{this.props.pageChange("start",null)}}  >Main Menu</h1>
+            <div className="col-1"/>
+            <div className="col-10 bb mt10 b10px  pb20px">
 
-     </div>
+
+                <div className="row">
+
+                  <div className="col-6">
+
+                    <div className="row">
+                        <div className="col-6">
+                          <img src = "images/boo1.png" className="w100"/>
+                        </div>
+                          <div className="col-6">
+                            <br />
+                            <h2 className="cy mt10 text-center">{0}</h2>
+                          </div>
+
+                    </div>
+
+                  </div>
+
+                  <div className="col-6">
+
+                    <div className="row">
+                        <div className="col-6">
+                          <img src = "images/boo1.png" className="w100"/>
+                        </div>
+                          <div className="col-6">
+                            <br />
+                            <h2 className="cy mt10 text-center">{0}</h2>
+                          </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+
+
+
+        </div>
+          <br />
+          <br />
+          <br />
+          <h1 className="backToMapTxt text-center mt10 sigmoreFont cy "  onClick = {()=>{this.props.pageChange("start",null)}}>Main Menu</h1>
+      </div>
+
 
    )
  }
